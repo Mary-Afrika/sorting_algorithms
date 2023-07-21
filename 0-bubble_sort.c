99584b7ab9fd16296d715a24f446a25eb5571613
kmishmael <kmishmael@gmail.com>
2023-07-21 23:31:15 +0300

    fix: break if loop is clean

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,22 +26,29 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t c;
-	int swap;
+	int temp;
+	int did_swap;
 
 	if (array == NULL || size == 0 || size == 1)
 		return;
 
 	for (c = 0; c  < size - 1; c++)
 	{
+		did_swap = 0;
 		for (i = 0; i < size - c - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap = array[i];
+				temp = array[i];
 				array[i] = array[i + 1];
-				array[i + 1] = swap;
+				array[i + 1] = temp;
+				did_swap = 1;
 				print_array(array, size);
 			}
+		}
+		if (!did_swap)
+		{
+			break;
 		}
 	}
 }
