668836a15ee3ac22e2a60534bafc01a48bf7eefd
Mary-Afrika <marygahar@gmail.com>
2023-07-21 12:51:14 -0700

    ALX

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
  * bubble_sort - sorts arrays of integers from min to max order
  * @array: array of unsorted integers
  * @size: size of the array
